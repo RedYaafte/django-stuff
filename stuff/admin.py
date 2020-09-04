4fd26e4a451a80d8d6f52ff313b3739c1b219f0a
Yafté M <redyafte@gmail.com>
2020-09-03 21:44:19 -0500

    Added UuId model

--- stuff/admin.py
+++ stuff/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UuId
+
+
+class UuIdAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UuId, UuIdAdmin)
