62d51488e692d69e6ce2222bf228607ca1797d76
Yafté M <redyafte@gmail.com>
2020-09-04 10:33:12 -0500

    Rename to uuid

--- stuff/models.py
+++ stuff/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class UuId(models.Model):
-    name = models.CharField(max_length=50)
+    uuid = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
+        return self.uuid
 
 
 class Formulary(models.Model):
