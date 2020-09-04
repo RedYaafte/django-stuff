cbb065ceb81a9c23c386ea65361bc9b665943ead
Yafté M <redyafte@gmail.com>
2020-09-04 10:02:49 -0500

    Added formulary model

--- stuff/admin.py
+++ stuff/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import UuId
+from .models import UuId, Formulary
 
 
 class UuIdAdmin(admin.ModelAdmin):
     pass
 
 
+class FormularyAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(UuId, UuIdAdmin)
+admin.site.register(Formulary, FormularyAdmin)
--- stuff/models.py
+++ stuff/models.py
@@ -6,3 +6,12 @@ class UuId(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Formulary(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.URLField()
+    uuid = models.ManyToManyField(UuId, verbose_name='uuids')
+
+    def __str__(self):
+        return self.name
